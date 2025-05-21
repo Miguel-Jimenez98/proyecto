e6924392c160f0501b862594635b6c938f33a2b2
Miguel-Jimenez98 <miguejime9810@gmail.com>
2025-05-20 19:19:42 -0500

    Se ha inicializado el repositorio

---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+Imagina que esta API es una biblioteca de películas:
+La función load_movies() es como un bibliotecario que carga el catálogo de libros (películas)
+cuando se abre la biblioteca.
+La función get_movies() muestra todo el catálogo cuando alguien lo pide.
+La función get_movie(id) es como si alguien preguntará por un libro en específico
+por su código de identificación.
+La función chatbot (query) es un asistente que busca películas según palabras clave y sinónimos.
+La función get_movies_by_category (category) ayuda a encontrar películas según su género (acción, comedia,)   
+"""
+#Importamos las herramientas necesarias para continuar nuestra API
+from fastapi import FastAPI, HTTPException # FastAPI nos ayuda a crear la API, HTTPException nos ayuda
+from fastapi.responses import HTMLResponse, JSONResponse # HTMLResponse nos ayuda a manejar respuestas HTML,
+import pandas as pd # Pandas nos ayuda a manejar datos en tablas como si fuera un Excel
+import nltk # nltk es una librería para procesar texto y analizar palabras.
+from nltk.tokenize import word_tokenize # word tokenize nos ayuda a tokenizar texto, es decir, a convertirlo en palabras
+from nltk.corpus import wordnet # Es una librería para analizar sinónimos
+
+#Indicamos la ruta donde NLTK buscará los datos descargados en nuestro computador
+nltk.data.path.append("C:\Users")
+nltk.download('punkt') #Es un paquete para dividir frases en palabras
+nltk.download('wordnet') #Paquete para encontrar sinónimos en palabras
+
+#Función para cargar las películas desde un archivo csv
+def load_movies():
+    #Leemos el archivo que contiene información de películas y seleccionamos las columnas más importantes
+    df = pd.read_csv("./Dataset/netflix_titles.csv")[{'show_id','title','release_year','listed_in','rating','description'}]
+    
+    #Renombramos las columnas para que sean más fáciles de entender
+    df.columns = ['id','tile','year','genres','rating','overview']
+    
+    #Llenamos los espacios vacios con texto vacio y convertimos los datos en una lista de diccionarios
+    return df.fillna('').to_dict(orient='records')
+    
+# Cargamos las películas al iniciar la API para no leer el archivo cada vez que alguien pregunte por ellas
+movies_list = load_movies()
+
+# Función para encontrar sinónimos de una palabra
+def get_synonyms(word):
+    # Usamos wordnet para encontrar distintas palabras que significan lo mismo
+    return{lemma.name().lower() for syn in wordnet.synsets(word) for lemma in syn.lemmas()}
+
+# Creamos la aplicación Fastapi, que será el motor de nuestra API
+# Esto inicializa la API con una versión
